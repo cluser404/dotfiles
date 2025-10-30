b9cd7b14e8d05fc38eaa7d99c2f243ae53f921df
cluser404 <30370919+cluser404@users.noreply.github.com>
2025-10-30 22:50:49 +0600

    added nvim keymaps for buffer and telescope

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -1,2 +1,11 @@
 
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, {desc = "show line diagnostics"} )
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = "Show line diagnostics" } )
+
+local telescope = require("telescope.builtin")
+vim.keymap.set('n', '<leader>ff', telescope.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', telescope.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', telescope.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', telescope.help_tags, { desc = 'Telescope help tags' })
+
+vim.keymap.set('n', '<Tab>', '<cmd>bnext<CR>', { desc = 'Goto next buffer' })
+vim.keymap.set('n', '<S-Tab>', '<cmd>bprevious<CR>', { desc = 'Goto previous buffer' })
